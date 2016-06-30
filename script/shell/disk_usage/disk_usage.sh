c867ded846db94426c4c40508ab644b0218f79b4
LiTianjue <mhpmii@126.com>
2016-06-30 16:40:17 +0800

    添加一个磁盘监控的脚本

---
+++ script/shell/disk_usage/disk_usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+max=30
+log_dir=/home/andy/tmp
+file_type=*.log
+del_args1=-mtime
+del_args2=+2
+
+percent=`df /var | tail -1 | awk '{print $5 }' | cut -d'%' -f1`
+
+echo "disk usage : $percent%"
+
+if [ "${percent}" -ge "${max}" ]; then
+	echo "need to remove something"
+	find $log_dir -name $file_type $del_args1 $del_args2 -exec rm -rf {} \;
+
+else
+	echo "disk run OK"
+fi;
